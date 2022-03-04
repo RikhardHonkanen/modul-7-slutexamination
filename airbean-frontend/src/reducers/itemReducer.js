a422f617de4bf59ba4aea3e39b5e0f9f89f7b118
RikhardHonkanen <75729661+RikhardHonkanen@users.noreply.github.com>
2022-03-04 20:20:54 +0100

    Prep Redux state

---
+++ airbean-frontend/src/reducers/itemReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+    counter: 0
+}
+
+const itemReducer = (state = initialState, action) => {
+    // https://medium.com/swlh/few-ways-to-update-a-state-array-in-redux-reducer-f2621ae8061
+    // https://redux.js.org/usage/structuring-reducers/initializing-state
+    // https://medium.com/@sqnguyen829/setting-up-multiple-reducers-for-react-redux-3d6566d4348f
+}
+
+export default itemReducer;
