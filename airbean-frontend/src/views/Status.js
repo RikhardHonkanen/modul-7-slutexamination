9981e42d02f64323b70a1e6949abb1e3da57680a
RikhardHonkanen <75729661+RikhardHonkanen@users.noreply.github.com>
2022-03-05 16:04:43 +0100

    Status page start

--- airbean-frontend/src/views/Status.js
+++ airbean-frontend/src/views/Status.js
@@ -1,6 +1,20 @@
 import '../App.css';
+import { useEffect } from 'react';
 
 function Status() {
+    useEffect(() => {
+        getStatus();
+    }, []);
+
+    async function getStatus() {
+        let url = "http://localhost:5000/api/beans"
+        const response = await fetch (url, {
+            method:"POST"
+        });
+        const confirmation = await response.json();
+        console.log(confirmation);
+    }
+
     return (
         <div className='status'>
             <h2>Order confirmation</h2>
